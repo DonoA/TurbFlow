dc2c63bb8c13984a8084c24961d3f9433338cd1f
DonoA <dallen@dallen.io>
2016-11-25 22:38:16 -0700

    it slows down at ~+-Pi/2 and idk why :(

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ var particles = [];
 var eddies = [];
 
 var debug = {
-  eddies : false,
-  turb: false,
-  autoSpawn: true
+  eddies : !false,
+  turb: !false,
+  autoSpawn: !true,
+  tick: true
 };
 
 function init(){
@@ -25,7 +26,7 @@ function init(){
   addParticle();
   drawFrame();
   setInterval(tick, 10);
-  for(var i = 0; i <= 10; i = i+1){
+  for(let i = 0; i <= 10; i = i+1){
     let rad1 = Math.random()*canHigh*0.07+canHigh*0.07;
     let rad2 = Math.random()*canHigh*0.1+rad1;
     let newEd;
@@ -68,6 +69,25 @@ function drawFrame(){
   pen.moveTo(canWid*0.1, canHigh*0.9);
   pen.lineTo(canWid*0.9, canHigh*0.9);
   pen.stroke();
+  //Debug eddies
+  if(debug.eddies){
+    eddies.forEach(function(e){
+      pen.fillStyle = "#800080";
+      pen.strokeStyle = "#800080";
+      pen.beginPath();
+      pen.arc(e.pos.x, e.pos.y, e.r2, 0, Math.PI*2, false);
+      pen.closePath();
+      pen.stroke();
+      pen.fill();
+      pen.fillStyle = "#000000";
+      pen.strokeStyle = "#000000";
+      pen.beginPath();
+      pen.arc(e.pos.x, e.pos.y, e.r1, 0, Math.PI*2, false);
+      pen.closePath();
+      pen.stroke();
+      pen.fill();
+    });
+  }
   //Draw particles
   particles.forEach(function(e){
     pen.fillStyle = "#ff0000";
@@ -78,20 +98,13 @@ function drawFrame(){
     pen.stroke();
     pen.fill();
   });
-  if(debug.eddies){
-    eddies.forEach(function(e){
-      pen.fillStyle = "#000000";
-      pen.strokeStyle = "#000000";
-      pen.beginPath();
-      pen.arc(e.pos.x, e.pos.y, 2, 0, Math.PI*2, false);
-      pen.closePath();
-      pen.stroke();
-      pen.fill();
-    });
-  }
 }
 
 function tick(){
+  if(!debug.tick){
+    drawFrame();
+    return;
+  }
   particles.forEach(function(e){
     e.pos.x = e.pos.x + e.vec.x;
     e.pos.y = e.pos.y + e.vec.y;
@@ -100,7 +113,7 @@ function tick(){
   particles = particles.filter(function(e){
     return e.pos.x < canWid*0.9-15;
   });
-  if(Math.random() <= 0.05 && debug.autoSpawn){ // 1% chance
+  if(Math.random() <= 0.05 && debug.autoSpawn){ // 5% chance
     addParticle();
   }
   drawFrame();
@@ -130,46 +143,68 @@ function addParticle(){
 function calcVec(pos){
   let r = (Math.abs(pos.y-canHigh*0.5))/(canHigh*0.5);
   let mag = vMag(r);
-  if(vcrit() > mag || debug.turb){
+  if(vcrit() > mag && !debug.turb){
     return {
       x: mag,
       y: 0
     };
   }else{
-    //This is all that still needs to be done
-    let handled = false;
-    eddies.forEach(function(e){
-      let dst = dist(pos, e.pos);
 
+    //This is all that still needs to be done
+    for(let i = 0; i < eddies.length; i = i+1){
+      let e = eddies[i];
+      let dst = dist(pos, e.pos);
       if(dst < e.r1){
-        // circle
-        let theta = Math.asin(dst/Math.abs(pos.y-e.pos.y));
-        if(pos.x > e.pos.x){
-          theta = Math.PI - theta;
+        console.log("circ");
+        let theta = Math.acos((pos.x-e.pos.x)/dst);
+        if(pos.y > e.pos.y){
+          theta = (Math.PI*2) - theta;
         }
-        return {
-          x: -mag*Math.sin(theta),
-          y: mag*Math.cos(theta)
-        };
-      }else if(dst < e.r2){
-        // push around
-        let theta = Math.asin(dst/Math.abs(pos.y-e.pos.y));
-        if(pos.x > e.pos.x){
-          theta = Math.PI - theta;
-        }
-        if(theta < Math.PI/10 || theta > Math.PI*(19/10)){
-          return {
-            x: (-mag*Math.sin(theta))*10,
-            y: (mag*Math.cos(theta))/10
-          };
-        }else{
+        console.log(theta/Math.PI);
+        debug.tick = false;
+        if(theta < Math.PI){
           return {
             x: -mag*Math.sin(theta),
             y: mag*Math.cos(theta)
           };
+        }else{
+          return {
+            x: mag*Math.sin(theta),
+            y: -mag*Math.cos(theta)
+          };
+        }
+      }else if(dst < e.r2){
+        console.log("push");
+        let theta = Math.acos((pos.x-e.pos.x)/dst);
+        if(pos.y > e.pos.y){
+          theta = (Math.PI*2) - theta;
+        }
+        console.log(theta/Math.PI);
+        // debug.tick = false;
+        if(Math.abs(theta-Math.PI) < Math.PI/10 || Math.abs(theta) < Math.PI/10){
+          console.log("flat");
+          // debug.tick = false;
+          return {
+            x: -mag*Math.sin(theta),
+            y: mag*Math.cos(theta)
+          };
+        }else{
+          console.log("go");
+          if(theta < Math.PI){
+            return {
+              x: -mag*Math.sin(theta),
+              y: mag*Math.cos(theta)
+            };
+          }else{
+            return {
+              x: mag*Math.sin(theta),
+              y: -mag*Math.cos(theta)
+            };
+          }
         }
       }
-    });
+    }
+    console.log("return");
     return {
       x: mag,
       y: 0
